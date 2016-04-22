b67b2805c43829726335f3dc0f0adb9147919848
Edgar Grepo <Batman@Edgars-MBP.hsd1.wa.comcast.net>
2016-04-22 15:29:51 -0700

    my redbelt

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,7 +1,10 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 
-$route['default_controller'] = "Surveys";
+$route['default_controller'] = "Travels";
+$route['addplan'] = "Travels/addplan";
+$route['logout'] = "Travels/logout";
+// $route['process_form'] = "Surveys/process_form";
 $route['404_override'] = '';
 
 
